ea41af84a5cb874ec8a66b447fca47bcf25ce55e
Li-WeiXin <2522048902@qq.com>
2022-08-28 22:34:40 +0800

    更新自动打包上传脚本配置

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 # git remote add origin https://github.com/sanyuan0704/my_blog.git
 
-git push -f origin  master:master
+git push -f origin  git@github.com:Li-WeiXin/Li-WeiXin.github.io.git
 
 cd ..
 

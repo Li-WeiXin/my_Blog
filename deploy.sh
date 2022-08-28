1bc6623a4e4a91c3ab04e8e4ee417acd6185ea80
Li-WeiXin <2522048902@qq.com>
2022-08-28 22:44:55 +0800

    更新自动打包上传脚本配置

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,25 @@
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
+rm -rf ../my_BlogDist/public/*
+
+# 将build生成的dist目录拷贝至上一层目录中
+cp -rf /public ../my_BlogDist/
 
 # 进入生成的文件夹
-cd public
+cd ../my_BlogDist/public
 
 # git init
-git add .
+git init
+git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 # git remote add origin https://github.com/sanyuan0704/my_blog.git
 
-git push -f origin  git@github.com:Li-WeiXin/Li-WeiXin.github.io.git
+git push -f origin  git@github.com:Li-WeiXin/Li-WeiXin.github.io.git main
 
 cd ..
 

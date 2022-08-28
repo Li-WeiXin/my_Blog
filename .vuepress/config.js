dae1d2dbc1d3cd488ccd0e750c71702a0abe0043
Li-WeiXin <2522048902@qq.com>
2022-08-28 21:36:23 +0800

    初始化我的博客

---
+++ .vuepress/config.js
@@ -0,0 +1,83 @@
+const sidebar = require('./siderbar.js');
+module.exports = {
+  "title": "李叁則的博客",
+  "description": "李叁則的博客",
+  "dest": "public",
+  "base": "/",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/logo.jpg"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "plugins": ["@vuepress-reco/vuepress-plugin-comments", "vuepress-plugin-meting"],
+  "theme": "reco",
+  "themeConfig": {
+    "mode": 'light',
+    "subSidebar": 'auto',
+    "valineConfig": {
+      "appId": 'CJH2UzaT4V6LUEV0kEilXtQa-gzGzoHsz',
+      "appKey": 'LAn5WMtwG5gfADCdbluNvYab',
+    },
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      // { "text": '留言板', "link": '/blogs/views/messageBoard.html', "icon": 'reco-suggestion' },
+      {
+        "text": "GitHub",
+        "icon": "reco-github",
+        "link": "https://github.com/Li-WeiXin"
+      },
+    ],
+    sidebar,
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "目录索引"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签索引"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "李叁則",
+    "authorAvatar": "/author.png",
+    "record": "首页",
+    "startYear": "2022",
+    "smoothScroll": true
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroText: 李叁則的博客
+tagline: 一切伟大的行动和思想，都有一个微不足道的开始
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
+features:
+- title: Always
+  details: 每天进步一点点
+---

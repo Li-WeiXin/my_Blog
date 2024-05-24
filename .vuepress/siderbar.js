945938089e846f000917167f96d6dbdb62f13f04
liweixin <weixin.li@nio.com>
2024-05-24 09:56:00 +0800

    feat: 新增css模块

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,6 +1,6 @@
 const { createSideBarConfig } = require('./util')
 const JAVASCRIPT_PATH = '/blogs/javascript'
-// const CSS_PATH = '/blogs/css'
+const CSS_PATH = '/blogs/css'
 // const ALGORITHM_PATH = '/blogs/algorithm'
 const GIT_PATH = '/blogs/git'
 // const OTHER_PATH = '/blogs/other'
@@ -9,7 +9,7 @@ const PERFORM_PATH = '/blogs/performance'
 
 module.exports = {
   [JAVASCRIPT_PATH]: [createSideBarConfig('js相关', JAVASCRIPT_PATH)],
-  // [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
+  [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
   [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
   // [ALGORITHM_PATH]: [createSideBarConfig('算法', ALGORITHM_PATH)],
   [GIT_PATH]: [createSideBarConfig('git', GIT_PATH)],
---
+++ blogs/css/01_css奇思妙想.md
@@ -0,0 +1,12 @@
+---
+title: css 奇思妙想
+date: 2024-05-24
+categories:
+ - css
+tags:
+ - css
+sidebar: 'auto'
+---
+
+## css 奇思妙想
+1. bottom: 0 与 bottom: auto 的区别 ? 

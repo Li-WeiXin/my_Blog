3f4a6bdd39f09b4efd09cef4c59e14e843e086c6
liweixin <weixin.li@nio.com>
2024-09-18 10:55:12 +0800

    feat: 新增实现对象的可选类型&&暂时隐藏css专项

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,19 +1,18 @@
-const { createSideBarConfig } = require('./util')
-const JAVASCRIPT_PATH = '/blogs/javascript'
-const CSS_PATH = '/blogs/css'
+const { createSideBarConfig } = require('./util');
+// const CSS_PATH = '/blogs/css'
+const JAVASCRIPT_PATH = '/blogs/javascript';
+const TYPESCRIPT_PATH = '/blogs/typescript';
+const PERFORM_PATH = '/blogs/performance';
 // const ALGORITHM_PATH = '/blogs/algorithm'
-const GIT_PATH = '/blogs/git'
+const GIT_PATH = '/blogs/git';
 // const OTHER_PATH = '/blogs/other'
-const PERFORM_PATH = '/blogs/performance'
-const TYPESCRIPT_PATH = '/blogs/typescript'
-
 
 module.exports = {
+  // [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
   [JAVASCRIPT_PATH]: [createSideBarConfig('js相关', JAVASCRIPT_PATH)],
-  [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
+  [TYPESCRIPT_PATH]: [createSideBarConfig('ts专项', TYPESCRIPT_PATH)],
   [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
   // [ALGORITHM_PATH]: [createSideBarConfig('算法', ALGORITHM_PATH)],
   [GIT_PATH]: [createSideBarConfig('git', GIT_PATH)],
   // [OTHER_PATH]: [createSideBarConfig('工具', OTHER_PATH)],
-  [TYPESCRIPT_PATH]: [createSideBarConfig('ts专项', TYPESCRIPT_PATH)],
-}
+};
